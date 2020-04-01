e76005ebfcd508cf3f1bbc4eab7e92a495dc6e25
Rodrigo Acuña <rodrigo.acuna.f@gmail.com>
2020-03-31 21:04:57 -0600

    Initial commit

---
+++ src/test/kotlin/com/roacfe/msscbrewery/MsscBreweryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.roacfe.msscbrewery
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsscBreweryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
